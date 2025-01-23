558d3397bfb819d37bd8f297e82ba71419df6315
Anand Dhar Dwivedi <add0203>
2025-01-24 03:16:01 +0530

    light mode update

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -99,10 +99,23 @@ export const BentoGridItem = ({
             <img
               src={img}
               alt={img}
-              className={cn(imgClassName, "object-cover object-center")}
+              className={cn(
+                imgClassName,
+                "object-cover object-center w-full h-full"
+              )}
             />
           )}
         </div>
+
+        {/* <div className="w-full h-full absolute">
+          {img && (
+            <img
+              src={img}
+              alt={img}
+              className={cn(imgClassName, "object-cover object-center")}
+            />
+          )}
+        </div> */}
         <div
           className={`absolute right-0 -bottom-5 ${
             id === 5 && "w-full opacity-80"
@@ -129,10 +142,10 @@ export const BentoGridItem = ({
             "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
           )}
         >
-          <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
+          <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10 text-white">
             {description}
           </div>
-          <div className="font-sans text-lg lg:text-3xl max-w-96 font-bold z-10">
+          <div className="font-sans text-lg lg:text-3xl max-w-96 font-bold z-10 text-white">
             {title}
           </div>
 
