62cec4496ad80ab271301a5a2c1ce714af286206
Anand Dhar Dwivedi <add0203>
2025-02-21 23:20:03 +0530

    contact updates

--- app/careers/page.tsx
+++ app/careers/page.tsx
@@ -18,7 +18,9 @@ const Careers = () => {
             {/* Apply Now Button */}
             <div className="flex justify-center">
               <button className="px-6 py-2 sm:py-3 bg-gray-200 dark:bg-gray-600 text-black dark:text-white rounded-full font-semibold mb-10 hover:bg-gray-400 dark:hover:bg-gray-500 transition-colors duration-200">
-                Submit Your Resume
+                <a href="https://forms.gle/b17Ax8t96TZiVgqn7">
+                  Submit Your Resume
+                </a>
               </button>
             </div>
 
